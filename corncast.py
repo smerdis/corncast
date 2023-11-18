d1323d24a7b670303d6157f5d4ba10f0486d6c88
Arjun Mukerji <stormandstress@gmail.com>
2023-11-18 15:18:55 -0800

    Improved docstrings of Location class

--- corncast.py
+++ corncast.py
@@ -1,13 +1,29 @@
 class Location(object):
-    """Class that represents a location in the world."""
+    """Class that represents a location in the world.
+
+    Attributes
+    ----------
+    name : string
+        name of location
+    lat : float
+        latitude of location
+    lon : float
+        longitude of location
+    """
+
     def __init__(self, name, lat, lon):
         """Create a Location.
         
-        Params:
-        name    |   string, name of Location
-        lat     |   float, latitude of location
-        lon     |   float, longitude of location
+        Parameters
+        ----------
+        name : string
+            name of location
+        lat : float
+            latitude of location
+        lon : float
+            longitude of location
         """
+
         self._name = name
         self._lat = lat
         self._lon = lon
@@ -15,9 +31,14 @@ class Location(object):
     def get_obs(self, noaa, start, end):
         """Return weather observations nearest this location. 
         
-        Params:
-        noaa    |   NOAA SDK object
-        start   |   Datetime or Pandas Timestamp, beginning of period to get observations for
-        end     |   Datetime or Pandas Timestamp, end of period to get observations for
+        Parameters
+        ----------
+        noaa : object
+            noaa_sdk.NOAA api wrapper object
+        start : datetime
+            beginning of time period we want observations from
+        end : datetime
+            end of time period we want observations from
         """
+
         return noaa.get_observations_by_lat_lon(self._lat, self._lon, start.strftime('%Y-%m-%d %H:%M:%S'), end.strftime('%Y-%m-%d %H:%M:%S'))
