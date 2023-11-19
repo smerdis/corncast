336db1bc7601a034c918286ca9f38dad5c99f6e9
Arjun Mukerji <stormandstress@gmail.com>
2023-11-19 11:18:03 -0800

    Fixed graph
    Converted the timestamp to a datetime column explicitly.

--- corncast.py
+++ corncast.py
@@ -1,3 +1,15 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.dates as mdates
+
+from dateutil import parser, tz
+from datetime import datetime, date, time, timedelta
+
+from noaa_sdk import NOAA
+
 class Location(object):
     """Class that represents a location in the world.
 
@@ -80,3 +92,44 @@ def freezethaw_yn(obs_df):
     
     else:
         return False
+    
+def corn_forecast(loc):
+    """Corn Forecast function.
+    
+    Parameters
+    ----------
+    loc : Location
+        Location this corn forecast is for
+    """
+
+    n = NOAA(user_agent="CornCast testing <arjmukerji@gmail.com>", show_uri=True)
+
+    now = datetime.now(tz=tz.UTC)
+    obs_period = timedelta(days=5)
+    start = now-obs_period
+    end = now
+
+    obs_list = [pd.json_normalize(o) for o in loc.get_obs(n, start, end)]
+    obs_df_full = pd.concat(obs_list, ignore_index=True)
+    obs_df_reduced = reduce_obs(obs_df_full).dropna(axis=0, subset=['temperature.value'])
+    obs_df_reduced.timestamp = pd.to_datetime(obs_df_reduced.timestamp)
+    assert(len(obs_df_reduced.station.unique())==1)
+    station_name = obs_df_reduced.station.iloc[0]
+
+    disp_cols = ['station', 'timestamp', 'temperature.value', 'dewpoint.value', 'precipitationLast3Hours.value']
+
+    df = obs_df_reduced
+    sns.lineplot(data=df[::-1], x='timestamp', y='temperature.value')
+    ax = plt.gca()
+    #locator = ticker.MultipleLocator(base=20)
+    locator = mdates.HourLocator(byhour=[6,18])
+    ax.xaxis.set_major_locator(locator)
+    ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation=45, ha='right')
+    ax.set_xlabel("Date and time")
+    ax.set_ylabel("Air Temperature (C)")
+    ax.set_title(f"{station_name} observations")
+    plt.show()
+    plt.close('all')
+
+
+    return df[disp_cols]
